b239083440b0f178971f95ca9ea71352c4758729
Ayush451 <88429944+Ayush451@users.noreply.github.com>
2023-01-12 21:39:24 +0000

    Template and removed unwanted file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import useLocalStorage from 'use-local-storage';
+import './index.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="login">
+        <h1>Login</h1>
+        <div className="container">
+          <div className="top">
+            <i class="fab fa-google"></i>
+            <i class="fab fa-facebook"></i>
+            <i class="fab fa-linkedin"></i>
+            <i class="fab fa-twitter-square"></i>
+            <i class="fab fa-apple"></i>
+          </div>
+          <p className="divider"><span>Or</span></p>
+          <form>
+            <label>E-mail</label>
+            <input type='email' placeholder="Enter your Email"/>
+            <label>Password</label>
+            <input type='Password' placeholder="Enter your Password"/>
+            <div className="remember">
+              <input type='checkbox' checked='checked'/>
+              <p>Remeber Me</p>
+            </div>
+            <button>Log In</button>
+          </form>
+          <div className="bottom">
+            <p>Forget your password?</p>
+            <a href="/">Reset Password</a>
+          </div>
+          <p className="create">Create Account</p>
+        </div>
+        <div className="theme-toggle">
+            <h2>Light Theme</h2>
+            <i class='fas fa-toggle-on'></i>
+        </div>
+      </div>
     </div>
   );
 }
